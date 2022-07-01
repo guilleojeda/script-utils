2c25a00798aa94eedec03b3f3048cf4b25987579
guilleojeda <gojeda@dreamoncloud.com>
2022-07-01 19:32:48 -0300

    Update ssh-tunnel-rds-ssm.sh

--- security/ssh-tunnel-rds-ssm.sh
+++ security/ssh-tunnel-rds-ssm.sh
@@ -1,5 +1,6 @@
 #first you need to create the temp ssh key
 #then replace all placeholders (yes, this should be parametrized)
+#then you need to log in to the AWS CLI
 aws ec2-instance-connect send-ssh-public-key --instance-id [instance-id] --availability-zone [availability-zone] --instance-os-user ssm-user --ssh-public-key file://temp.pub
 ssh -i temp -N -f -M -S temp-ssh.sock -L [local-port]:[db-host]:[db-port] ssm-user@[instance-id] -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -o ProxyCommand="aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters portNumber=%p"
 read -rsn1 -p "Press any key to close session."; echo
